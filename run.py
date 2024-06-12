6cd9d6aeb8208ecfaefc5946af59d1bfc311efda
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2024-06-12 21:58:52 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,26 @@
+from deepface import DeepFace
+DeepFace.stream("my_db")
+
+# img_path = "aa.jpg"
+
+# img = cv2.imread(img_path)
+
+# plt.imshow(img[:,:, ::-1])
+# plt.show()
+
+# resp = DeepFace.analyze(img_path)
+
+# print("     ")
+# print("     ")
+
+# print(resp[1])
+
+# objs = DeepFace.analyze(
+#   img_path = "aa.jpg", 
+#   actions = ['age', 'gender', 'race', 'emotion'],
+# )
+
+# print(objs)
+
+# [:, :, ::-1] , 'gender', 'race', 'emotion'
+
